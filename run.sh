e6d4d9f334ae30ab8bf504d51661efe0eb5dffe6
Slowerzs <slowerzs@hackademint.org>
2021-08-11 21:24:28 +0200

    fix path in run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 PATH=$(/usr/bin/getconf PATH || /bin/kill $$)
 
 docker stop $(docker ps -a -q --filter ancestor=rootmebot --format="{{.ID}}")
-docker build -t RootMeBot rootmebot --no-cache
-docker run rootmebot -v `pwd`/db:/opt/RootMeBot/db 
+docker build -t rootmebot . --no-cache
+docker run rootmebot -v `pwd`/db:/opt/db 
 
